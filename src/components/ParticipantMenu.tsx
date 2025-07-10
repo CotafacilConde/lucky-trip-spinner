6be2a46df2cba842a89ff708d63a0dc4b4ec76cc
CotafacilConde <cotafacilcondemkt@gmail.com>
2025-07-10 02:33:42 -0300

    Update ParticipantMenu.tsx

--- src/components/ParticipantMenu.tsx
+++ src/components/ParticipantMenu.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import {
   DropdownMenu,
@@ -18,18 +17,32 @@ const ParticipantMenu: React.FC<ParticipantMenuProps> = ({ onEdit, onDelete }) =
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="h-8 w-8 p-0">
+        <Button 
+          variant="ghost" 
+          className="h-8 w-8 p-0 hover:bg-gray-100 focus:bg-gray-100 data-[state=open]:bg-gray-100"
+        >
+          <span className="sr-only">Abrir menu</span>
           <MoreHorizontal className="h-4 w-4" />
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={onEdit}>
-          <Edit className="mr-2 h-4 w-4" />
-          Editar
+      <DropdownMenuContent 
+        align="end" 
+        className="w-48 bg-white border border-gray-200 shadow-lg z-50"
+        sideOffset={5}
+      >
+        <DropdownMenuItem 
+          onClick={onEdit}
+          className="cursor-pointer hover:bg-gray-50 focus:bg-gray-50 px-3 py-2"
+        >
+          <Edit className="mr-2 h-4 w-4 text-blue-600" />
+          <span className="text-gray-700">Editar</span>
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={onDelete} className="text-red-600">
+        <DropdownMenuItem 
+          onClick={onDelete} 
+          className="cursor-pointer hover:bg-red-50 focus:bg-red-50 px-3 py-2 text-red-600"
+        >
           <Trash2 className="mr-2 h-4 w-4" />
-          Excluir
+          <span>Excluir</span>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
